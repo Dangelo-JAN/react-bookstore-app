7757bcb048c90256d8bbdc8c27ad630cbc530081
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2022-04-29 16:20:04 -0400

    My 1st. commit

---
+++ src/components/BooksContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const BooksContainer = () => {
+
+  return(
+
+  )
+};
+
+export default BooksContainer;
---
+++ src/pages/Categories.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Categories = () => {
+  return(
+    <h1>categories</h1>
+  )
+}
+
+export default Categories;
