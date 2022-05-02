777c834482931fd07d31001674e7dd2a984650b0
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2022-05-01 21:55:33 -0400

    Create files and update index,js file

--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import BookForm from './BookForm';
 import BookItem from './BookItem';
 
 const BooksContainer = () => {
-  const bookItem = {
+  const [bookItem] = useState({
     id: 1,
     title: 'The Green Mile',
     author: 'Stephen King',
-  };
+  });
+
 
   const { title, author } = bookItem;
 
