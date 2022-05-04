07299111d56a9bb7ff528103a9d017e22327d047
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-05-04 17:01:47 -0400

    Create a book using the form

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,13 +1,54 @@
-const BookForm = () => (
-  <form action="submit">
-    <label htmlFor="title">
-      <input type="text" name="title" id="title" placeholder="Add Title..." />
-    </label>
-    <label htmlFor="title">
-      <input type="text" name="author" id="author" placeholder="Add Author..." />
-    </label>
-    <button onClick={(e) => { e.preventDefault(); }} type="submit">Add Book</button>
-  </form>
-);
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createBooks } from '../redux/BookList/BookList';
+
+const BookForm = () => {
+  const dispatch = useDispatch();
+  const [createBook, setCreateBook] = useState(
+    {
+      title: '',
+      author: '',
+    },
+  );
+  const { title, author } = createBook;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createBooks(title, author));
+  };
+
+  const onInputChange = (e) => {
+    setCreateBook({
+      ...createBook,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="title">
+        <input
+          type="text"
+          name="title"
+          id="title"
+          value={title}
+          placeholder="Add Title..."
+          onChange={onInputChange}
+        />
+      </label>
+      <label htmlFor="title">
+        <input
+          type="text"
+          name="author"
+          id="author"
+          value={author}
+          placeholder="Add Author..."
+          onChange={onInputChange}
+        />
+      </label>
+      <button type="submit">Add Book</button>
+    </form>
+  );
+};
 
 export default BookForm;
